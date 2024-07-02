38d25b39040b927ea77f516c4faa9df1a8f7f8c1
JulioGroaz <julio.groaz2020@gmail.com>
2024-07-02 13:57:40 +0200

    Stampo a schermo

--- index.php
+++ index.php
@@ -4,4 +4,7 @@
 
     $movie1 = new Movie("Inception", "Un thriller che sfida la mente su sogni dentro sogni.", 9);
     $movie2 = new Movie("The Matrix", "Un futuro distopico dove la realtà non è come sembra.", 10);
+
+    echo $movie1->getDescription() . "<br>";
+    echo $movie2->getDescription() . "<br>";
 ?>
