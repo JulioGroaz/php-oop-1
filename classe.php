c922ba16022b3502098e560f740fd2d0c8b9aa99
JulioGroaz <julio.groaz2020@gmail.com>
2024-07-03 10:42:17 +0200

    Aggiunta estensione PHP DocBlocker e commentato il codice

--- classe.php
+++ classe.php
@@ -4,27 +4,53 @@ class Movie{
     public $nome;
     public $descrizione;
     public $voto;
-
+    /**
+     * Costruttore per la classe
+     *
+     * @param [string] $nome
+     * @param [string] $descrizione
+     * @param [int] $voto
+     */
     public function __construct($nome, $descrizione, $voto) {
         $this->nome = $nome;
         $this->descrizione = $descrizione;
         $this->voto = $voto;
     }
+    
 
+    /**
+     * Questa function ritorna la descrizione del film
+     *
+     * @return string Titolo, descrizione e voto
+     */
     public function getDescription() {
         return "Titolo: " . $this->nome . ", Descrizione: " . $this->descrizione . ", Voto: " . $this->voto . "/10";
     }
+    
 
+    /**
+     * Questa function restituisce il titolo del film
+     *
+     * @return string Il titolo del libro
+     */
     public function getTitle() {
         return $this->nome;
     }
 
-    
+    /**
+     * Questa function restituisce la descrizione del film
+     *
+     * @return string La descrizione del film
+     */
     public function getDescriptionText() {
         return $this->descrizione;
     }
 
-   
+    /**
+     * Questa function restituisce il voto del film
+     *
+     * @return int voto del film
+     */
     public function getRating() {
         return $this->voto;
     }
--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+    
     include 'Classe.php';
 
-
+    
     $movie1 = new Movie("Inception", "Un thriller che sfida la mente su sogni dentro sogni.", 9);
     $movie2 = new Movie("The Matrix", "Un futuro distopico dove la realtà non è come sembra.", 10);
-
+    
     echo $movie1->getDescription() . "<br>";
     echo $movie2->getDescription() . "<br>";
 ?>
